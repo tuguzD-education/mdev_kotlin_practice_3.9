6df488dfad0557d408083fe7113216a435940aec
Damir Tugushev <0damir.1tugushev@gmail.com>
2022-04-09 23:56:36 +0300

    Initial commit

---
+++ app/src/main/java/io/github/tuguzd/navigationapi2/MainActivity.kt
@@ -0,0 +1,113 @@
+package io.github.tuguzd.navigationapi2
+
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.ContextMenu.ContextMenuInfo
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.view.ActionMode
+import androidx.navigation.fragment.NavHostFragment
+import io.github.tuguzd.navigationapi2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var actionMode: ActionMode
+    private lateinit var actionModeCallback: ActionMode.Callback
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(binding.content.navHostFragmentContentMain.id) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        actionModeCallback = object: ActionMode.Callback {
+            override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                mode?.menuInflater?.inflate(R.menu.menu_context, menu)
+                return true
+            }
+
+            override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean = false
+
+            override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+                kotlin.run {
+                    val text = when (item?.itemId) {
+                        R.id.search -> "Search"
+                        R.id.gallery -> "Gallery"
+                        else -> return@run
+                    }
+                    Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+                }
+                mode?.finish()
+                return false
+            }
+
+            override fun onDestroyActionMode(p0: ActionMode?) = Unit
+        }
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+            startSupportActionMode(actionModeCallback)?.let {
+                actionMode = it
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        Toast.makeText(this, "MenuItem click!", Toast.LENGTH_SHORT).show()
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(binding.content.navHostFragmentContentMain.id)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        // menuInflater.inflate(R.menu.menu_main, menu)
+        menu?.add(Menu.NONE, 101, Menu.NONE, "Open")
+        menu?.add(Menu.NONE, 102, Menu.NONE, "Save")
+        menu?.add(Menu.NONE, 103, Menu.NONE, "Exit")
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(this, "ContextMenuItem click!", Toast.LENGTH_SHORT).show()
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
